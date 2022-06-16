b4a7a8e6a9b9eff0a98fdb671836bd5e15061cbf
Soroush <s_shemshadi@ymail.com>
2022-06-16 13:15:17 +0430

    INIT(SpringBoot2.7.0)

---
+++ src/main/java/io/github/shuoros/doitbot/DoItBotApplication.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.doitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/shuoros/doitbot/DoItBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.doitbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
