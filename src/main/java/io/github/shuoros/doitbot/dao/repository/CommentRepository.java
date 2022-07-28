1b11422c9d858dafd07199b34aed0adaf2524645
Soroush <s_shemshadi@ymail.com>
2022-07-28 15:29:52 +0430

    IMPLEMENT(Comment)

---
+++ src/main/java/io/github/shuoros/doitbot/dao/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package io.github.shuoros.doitbot.dao.repository;
+
+import io.github.shuoros.doitbot.dao.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, String> {
+}
