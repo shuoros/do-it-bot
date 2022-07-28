28c38f206d6843b292484e4cf8733564c3af799d
Soroush <s_shemshadi@ymail.com>
2022-07-28 15:41:23 +0430

    IMPLEMENT(robot.token properties file)

--- src/main/java/io/github/shuoros/doitbot/DoItBotApplication.java
+++ src/main/java/io/github/shuoros/doitbot/DoItBotApplication.java
@@ -2,8 +2,14 @@ package io.github.shuoros.doitbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:application-robot.properties")
+})
 public class DoItBotApplication {
 
     public static void main(String[] args) {
